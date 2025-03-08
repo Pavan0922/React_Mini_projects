b6329d57254c1f328f9446788ce50a58d6a4db01
Pavan0922 <pavankumarbm1234@gmail.com>
2025-03-08 16:25:28 +0530

    first commit

---
+++ Level-easy-colorPicker/src/App.jsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+
+function App() {
+  const [color, setColor] = useState("green");
+  return (
+    <div className="w-full h-screen" style={{ backgroundColor: color }}>
+      <div className="flex fixed flex-wrap justify-center bottom-10 inset-x-0 ">
+        <div className="flex justify-center flex-wrap bg-white p-5 rounded-full gap-4 ">
+          <button
+            onClick={() => setColor("red")}
+            className="bg-pink-400 px-8 py-2 rounded-full inset-y-0"
+          >
+            pink
+          </button>
+          <button
+            onClick={() => setColor("green")}
+            className="bg-green-600 px-8 py-2 rounded-full inset-y-0"
+          >
+            green
+          </button>
+          <button
+            onClick={() => setColor("black")}
+            className=" bg-black text-white px-8 py-2 rounded-full inset-y-0"
+          >
+            black
+          </button>
+          <button
+            onClick={() => setColor("white")}
+            className="bg-white-400 px-8 py-2 rounded-full inset-y-0"
+          >
+            white
+          </button>
+          <button
+            onClick={() => setColor("blue")}
+            className="bg-blue-400 px-8 py-2 rounded-full inset-y-0"
+          >
+            blue
+          </button>
+          <button
+            onClick={() => setColor("purple")}
+            className="bg-purple-400 px-8 py-2 rounded-full inset-y-0"
+          >
+            purple
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default App;
