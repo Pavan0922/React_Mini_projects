03d3b10a4f61a3f0df083d8d2bb45e866d7d970e
Pavan0922 <pavankumarbm1234@gmail.com>
2025-03-21 21:15:41 +0530

    minor changes

--- Level-medium-currencyConverter/currencyConverter/src/App.jsx
+++ Level-medium-currencyConverter/currencyConverter/src/App.jsx
@@ -3,25 +3,13 @@ import "./index.css";
 
 function CurrencyConvertor() {
   return (
-    <div className="currency-convertor">
-      <h2>Currency Convertor</h2>
-      <div className="input-section">
-        <input type="number" placeholder="Enter amount" />
-        <select>
-          <option value="USD">USD</option>
-          <option value="INR">INR</option>
-          {/* Add more currency options as needed */}
-        </select>
-        <span>to</span>
-        <select>
-          <option value="INR">INR</option>
-          <option value="USD">USD</option>
-          {/* Add more currency options as needed */}
-        </select>
-      </div>
-      <button>Convert</button>
-      <div className="result"></div>
-    </div>
+    <>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Exercitationem
+        quae quidem ad voluptatum, excepturi atque vero velit obcaecati debitis
+        eos.
+      </p>
+    </>
   );
 }
 
