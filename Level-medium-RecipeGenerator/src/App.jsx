fd302cc6a43a48eedc830500c083b16f891f595f
Pavan Kumar BM <pavankumarbm1234@gmail.como>
2025-05-12 20:52:34 +0530

    Changes in UI

--- Level-medium-RecipeGenerator/src/App.jsx
+++ Level-medium-RecipeGenerator/src/App.jsx
@@ -1,15 +1,41 @@
-import './App.css'
-import Header from './Components/Header'
-import Main from './Components/Main'
-function App() {
-  return (
-    <>
-    <div className="w-lg mx-auto  border-b-blue-400 border-2">
-      <Header/>
-      <Main/>
-    </div>        
-    </>
-  )
-}
+import "./App.css";
+import { useState } from "react";
+export default function Main() {
+  /**
+   * Challenge: Update our app so that when the user enters a
+   * new ingredient and submits the form, it adds that new
+   * ingredient to our list!
+   */
+  const [ingredients, setingredients] = useState([]);
 
-export default App
+  const ingredientsListItems = ingredients.map((ingredient) => (
+    <li key={ingredient}>{ingredient}</li>
+  ));
+
+  function handleSubmit(event) {
+    /**
+     * Like before, don't worry about this FormData stuff yet.
+     * Just use the newIngredient below to help you finish the
+     * challenge.
+     */
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const newIngredient = formData.get("ingredient");
+    setingredients((prev) => [...prev, newIngredient]);
+  }
+
+  return (
+    <main>
+      <form onSubmit={handleSubmit} className="add-ingredient-form">
+        <input
+          type="text"
+          placeholder="e.g. oregano"
+          aria-label="Add ingredient"
+          name="ingredient"
+        />
+        <button>Add ingredient</button>
+      </form>
+      <ul>{ingredientsListItems}</ul>
+    </main>
+  );
+}
